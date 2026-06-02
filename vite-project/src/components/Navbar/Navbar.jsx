c829a8cc4b4b44e80adf7e1c567385e9a3ded2c5
scarls00 <seritarak@gmail.com>
2026-06-02 17:06:56 -0500

    update styling and layout

--- vite-project/src/components/Navbar/Navbar.jsx
+++ vite-project/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 import search_icon from '../../assets/search_icon.svg'
@@ -7,9 +7,23 @@ import profile_img from '../../assets/profile_img.png'
 import caret_icon from '../../assets/caret_icon.svg'
 
 
-function Navbar() {
+const Navbar = () => {
+
+  const navRef = useRef();
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= 80){
+        navRef.current.classList.add('nav-dark')
+      }
+      else {
+        navRef.current.classList.remove('nav-dark')
+      }
+    })
+  },[])
+
   return (
-    <div className='navbar'>
+    <div ref={navRef}className='navbar'>
       <div className="navbar-left">
         <img src={logo} alt="Logo" />
         <ul>
--- vite-project/src/pages/Player/Player.jsx
+++ vite-project/src/pages/Player/Player.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Player.css'
+import back_arrow_icon from "../../assets/back_arrow_icon.png"
+import { useNavigate, useParams } from 'react-router-dom'
 
-const Player = () => {
+const Player = () => { 
+
+  const {id} = useParams();
+  const navigate = useNavigate();
+
+  const [apiData, setApiData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    type: "",
+  })
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNDE4ZmU3YTU5NGIwYjRmMmMwNGU3OWRiMTdhNWYzYyIsIm5iZiI6MTc4MDQxODU2NC44OTc5OTk4LCJzdWIiOiI2YTFmMDgwNDA3OTY5MzAzNmM4YTAyZjUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.M4M5t0dJo_FWCewp2lngFsF3C0kIUwcBV-bKMFYir90'
+    }
+  }
+
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+      .then(res => res.json())
+      .then(res => {
+        if (res && res.results && res.results[0]) {
+          setApiData(res.results[0])
+        }
+      })
+      .catch(err => console.error(err));
+  }, [])
+
   return (
     <div className='player'>
-      <h1>Now Playing</h1>
+      <img src={back_arrow_icon} alt="" onClick={() => {navigate(-2)}} />
+      <iframe width='90%' height='90%' 
+        src={`https://www.youtube.com/embed/${apiData.key}`}
+        title='trailer' frameBorder='0' allowFullScreen></iframe>
+      <div className="player-info">
+        <p>{apiData.published_at.slice(0, 10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
+      </div>
     </div>
   )
 }
